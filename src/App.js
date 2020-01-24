ed614de4b2a5a5819d3659585ff1a7f4ce3b737d
Senipah <n.morgan86@googlemail.com>
2020-01-24 14:17:09 +0000

    adding components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Clock from './Components/Clock';
 
+// const projectName = 'pomodoro-clock';
+// localStorage.setItem('example_project', 'Pomodoro Clock');
+
 function App() {
   return (
     <div className='app'>
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,44 +1,77 @@
 import React from 'react';
 import './Clock.css';
+const { useReducer, createContext, useContext } = React;
 
-function Session() {
-  return (
-    <div id='session'>
-      <div id='session-label'>
-        <h2>Session Length</h2>
-      </div>
-      <div id='session-buttons'>
-        <button id='break-decrement'>Decrement</button>
-        <button id='break-increment'>Increment</button>
-      </div>
-    </div>
-  );
+const initialState = {
+  breakLength: 5,
+  sessionLength: 25,
+  timerVal: 25,
+  running: false,
+  onBreak: false
+};
+
+const ClockContext = React.createContext();
+
+function clockReducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
 
-function Break() {
-  return (
-    <div id='break'>
-      <div id='break-label'>
-        <h2>Break Length</h2>
-      </div>
-      <div id='break-buttons'>
-        <button id='break-decrement'>Decrement</button>
-        <button id='break-increment'>Increment</button>
-      </div>
-    </div>
-  );
-}
-
-function Clock() {
+export default function Clock() {
+  const [state, dispatch] = useReducer(clockReducer, initialState);
   return (
     <div id='clock'>
-      <div id='controls'>
-        <Break />
-        <Session />
-      </div>
-      <h1>I'm a clock</h1>
+      <ClockContext.Provider value={{ state: state, dispatch: dispatch }}>
+        <ControlGroup />
+        <Timer />
+      </ClockContext.Provider>
     </div>
   );
 }
 
-export default Clock;
+function Timer() {
+  const { state } = React.useContext(ClockContext);
+  return (
+    <div id='timer'>
+      <h2 id='timer-label'>{state.onBreak ? 'Session' : 'Break'}</h2>
+      <div id='time-left'>{state.timerVal}</div>
+      <div id='timer-controls'>
+        <button id='start_stop'>Start/Stop</button>
+        <button id='reset'>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+function Control(props) {
+  const { state } = React.useContext(ClockContext);
+  const titleCase =
+    props.name.charAt(0).toUpperCase() + props.name.slice(1).toLowerCase();
+  return (
+    <div id={props.name} className='control'>
+      <div id={`${props.name}-label`} className='control-label'>
+        <h2>{titleCase} Length</h2>
+        <div id={`${props.name}-length`} className='control-value'>
+          {state[`${props.name}Length`]}
+        </div>
+      </div>
+      <div id={`${props.name}-buttons`} className='control-buttons'>
+        <button id={`${props.name}-decrement`}>Decrement</button>
+        <button id={`${props.name}-increment`}>Increment</button>
+      </div>
+    </div>
+  );
+}
+
+function ControlGroup() {
+  const controls = ['break', 'session'];
+  return (
+    <div id='controls'>
+      {controls.map(e => (
+        <Control name={e} />
+      ))}
+    </div>
+  );
+}
