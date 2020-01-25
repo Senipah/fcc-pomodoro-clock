2d1ba3037d9ba44ccbbd3daa5543fbb871f0912e
Senipah <n.morgan86@googlemail.com>
2020-01-25 12:16:45 +0000

    fix reducer for controls

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -10,20 +10,13 @@ const initialState = {
   onBreak: false
 };
 
-const ClockContext = React.createContext();
-
-function clockReducer(state, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
+const ClockContext = createContext(initialState);
 
 export default function Clock() {
-  const [state, dispatch] = useReducer(clockReducer, initialState);
+  const state = useContext(ClockContext);
   return (
     <div id='clock'>
-      <ClockContext.Provider value={{ state: state, dispatch: dispatch }}>
+      <ClockContext.Provider value={state}>
         <ControlGroup />
         <Timer />
       </ClockContext.Provider>
@@ -32,7 +25,7 @@ export default function Clock() {
 }
 
 function Timer() {
-  const { state } = React.useContext(ClockContext);
+  const state = useContext(ClockContext);
   return (
     <div id='timer'>
       <h2 id='timer-label'>{state.onBreak ? 'Session' : 'Break'}</h2>
@@ -45,10 +38,49 @@ function Timer() {
   );
 }
 
+function controlReducer(state, action) {
+  const increment = val => {
+    return (val += 1);
+  };
+  const decrement = val => {
+    return (val -= 1);
+  };
+
+  const handleType = (val, type) => {
+    if (type === 'increment') {
+      return increment(val);
+    } else if (type === 'decrement') {
+      return decrement(val);
+    }
+    return val;
+  };
+
+  switch (action.name) {
+    case 'session':
+      return {
+        ...state,
+        sessionLength: handleType(state.sessionLength, action.type)
+      };
+    case 'break':
+      return {
+        ...state,
+        breakLength: handleType(state.breakLength, action.type)
+      };
+    default:
+      return state;
+  }
+}
+
 function Control(props) {
-  const { state } = React.useContext(ClockContext);
+  const [state, dispatch] = useReducer(
+    controlReducer,
+    useContext(ClockContext)
+  );
   const titleCase =
     props.name.charAt(0).toUpperCase() + props.name.slice(1).toLowerCase();
+  console.log(`${props.name}Length`);
+  console.log(state);
+  console.log(state[`${props.name}Length`]);
   return (
     <div id={props.name} className='control'>
       <div id={`${props.name}-label`} className='control-label'>
@@ -58,8 +90,18 @@ function Control(props) {
         </div>
       </div>
       <div id={`${props.name}-buttons`} className='control-buttons'>
-        <button id={`${props.name}-decrement`}>Decrement</button>
-        <button id={`${props.name}-increment`}>Increment</button>
+        <button
+          id={`${props.name}-decrement`}
+          onClick={() => dispatch({ ...props, type: 'decrement' })}
+        >
+          Decrement
+        </button>
+        <button
+          id={`${props.name}-increment`}
+          onClick={() => dispatch({ ...props, type: 'increment' })}
+        >
+          Increment
+        </button>
       </div>
     </div>
   );
