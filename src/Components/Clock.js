66058377758956a4148e8bb8ee1819d08c68fb7b
Senipah <n.morgan86@googlemail.com>
2020-01-31 14:13:40 +0000

    passing all tests

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Clock.css';
-const { useReducer, createContext, useContext } = React;
+import alarm from '../assets/audio/alertklaxon_clean.mp3';
+const { useReducer, createContext, useContext, useEffect, useRef } = React;
 
 const initialState = {
   breakLength: 5,
   sessionLength: 25,
-  timerVal: 25,
+  timerMinutes: 25,
+  timerSeconds: 0,
   running: false,
-  onBreak: false
+  isSession: true
 };
 
-const ClockContext = createContext(initialState);
+const ClockContext = createContext();
 
 export default function Clock() {
-  const state = useContext(ClockContext);
+  const [state, dispatch] = useReducer(clockReducer, initialState);
   return (
     <div id='clock'>
-      <ClockContext.Provider value={state}>
+      <ClockContext.Provider value={{ state, dispatch }}>
         <ControlGroup />
         <Timer />
       </ClockContext.Provider>
@@ -24,21 +26,7 @@ export default function Clock() {
   );
 }
 
-function Timer() {
-  const state = useContext(ClockContext);
-  return (
-    <div id='timer'>
-      <h2 id='timer-label'>{state.onBreak ? 'Session' : 'Break'}</h2>
-      <div id='time-left'>{state.timerVal}</div>
-      <div id='timer-controls'>
-        <button id='start_stop'>Start/Stop</button>
-        <button id='reset'>Reset</button>
-      </div>
-    </div>
-  );
-}
-
-function controlReducer(state, action) {
+function clockReducer(state, action) {
   const increment = val => {
     return (val += 1);
   };
@@ -48,39 +36,90 @@ function controlReducer(state, action) {
 
   const handleType = (val, type) => {
     if (type === 'increment') {
-      return increment(val);
+      const newVal = increment(val);
+      return newVal > 60 ? val : newVal;
     } else if (type === 'decrement') {
-      return decrement(val);
+      const newVal = decrement(val);
+      return newVal < 1 ? val : newVal;
     }
     return val;
   };
 
+  const tick = state => {
+    const SECONDS_BOUND = 59;
+    let { timerMinutes, timerSeconds } = state;
+    timerSeconds = decrement(timerSeconds);
+    if (timerSeconds < 0) {
+      timerMinutes = decrement(timerMinutes);
+      if (timerMinutes < 0) {
+        //  interval change
+        if (state.isSession) {
+          timerMinutes = state.breakLength;
+        } else {
+          timerMinutes = state.sessionLength;
+        }
+        timerSeconds = initialState.timerSeconds;
+        state.alert.current.play();
+        return {
+          ...state,
+          timerMinutes: timerMinutes,
+          timerSeconds: timerSeconds,
+          isSession: !state.isSession
+        };
+      }
+      return {
+        ...state,
+        timerMinutes: timerMinutes,
+        timerSeconds: SECONDS_BOUND
+      };
+    }
+    return {
+      ...state,
+      timerSeconds
+    };
+  };
+
+  let newState = {};
   switch (action.name) {
     case 'session':
-      return {
-        ...state,
-        sessionLength: handleType(state.sessionLength, action.type)
-      };
+      const newSessionLength = handleType(state.sessionLength, action.type);
+      if (state.running) {
+        newState = {
+          ...state,
+          sessionLength: newSessionLength
+        };
+      } else {
+        newState = {
+          ...state,
+          timerMinutes: newSessionLength,
+          sessionLength: newSessionLength
+        };
+      }
+      return newState;
     case 'break':
-      return {
+      const newBreakLength = handleType(state.breakLength, action.type);
+      newState = {
         ...state,
-        breakLength: handleType(state.breakLength, action.type)
+        breakLength: newBreakLength
       };
+      return newState;
+    case 'start_stop':
+      return { ...state, running: !state.running };
+    case 'reset':
+      state.alert.current.pause();
+      state.alert.current.currentTime = 0;
+      return initialState;
+    case 'tick':
+      return tick(state);
     default:
       return state;
   }
 }
 
 function Control(props) {
-  const [state, dispatch] = useReducer(
-    controlReducer,
-    useContext(ClockContext)
-  );
+  const { state, dispatch } = useContext(ClockContext);
   const titleCase =
     props.name.charAt(0).toUpperCase() + props.name.slice(1).toLowerCase();
-  console.log(`${props.name}Length`);
-  console.log(state);
-  console.log(state[`${props.name}Length`]);
   return (
     <div id={props.name} className='control'>
       <div id={`${props.name}-label`} className='control-label'>
@@ -92,13 +131,13 @@ function Control(props) {
       <div id={`${props.name}-buttons`} className='control-buttons'>
         <button
           id={`${props.name}-decrement`}
-          onClick={() => dispatch({ ...props, type: 'decrement' })}
+          onClick={() => dispatch({ name: props.name, type: 'decrement' })}
         >
           Decrement
         </button>
         <button
           id={`${props.name}-increment`}
-          onClick={() => dispatch({ ...props, type: 'increment' })}
+          onClick={() => dispatch({ name: props.name, type: 'increment' })}
         >
           Increment
         </button>
@@ -111,9 +150,62 @@ function ControlGroup() {
   const controls = ['break', 'session'];
   return (
     <div id='controls'>
-      {controls.map(e => (
-        <Control name={e} />
+      {controls.map((e, i) => (
+        <Control name={e} key={i} />
       ))}
+    </div>
+  );
+}
+
+function Timer() {
+  const { state, dispatch } = useContext(ClockContext);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (state.running) {
+        dispatch({ name: 'tick' });
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dispatch, state.running]);
+
+  const formatTime = val => {
+    if (val < 0) {
+      return '00';
+    }
+    if (val < 10) {
+      return '0' + val;
+    }
+    return val;
+  };
+
+  const alert = useRef(null);
+  state['alert'] = alert;
+  // const AlertURL =
+  //   'https://www.trekcore.com/audio/redalertandklaxons/alertklaxon_clean.mp3';
+
+  return (
+    <div id='timer'>
+      <audio src={alarm} ref={alert} className='clip' id='beep' />
+      <h2 id='timer-label'>{state.isSession ? 'Session' : 'Break'}</h2>
+      <div id='time-left'>
+        <span>{formatTime(state.timerMinutes)}</span>
+        <span>:</span>
+        <span>{formatTime(state.timerSeconds)}</span>
+      </div>
+      <div id='timer-controls'>
+        <button
+          id='start_stop'
+          onClick={() => dispatch({ name: 'start_stop' })}
+        >
+          Start/Stop
+        </button>
+        <button id='reset' onClick={() => dispatch({ name: 'reset' })}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
