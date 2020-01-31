8d379036a1ffeeff547c4f809155b20847f0c250
Senipah <n.morgan86@googlemail.com>
2020-01-31 18:22:43 +0000

    track build

---
+++ build/precache-manifest.3f3f5ae460def592716eb4c0d26b2544.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1e0478fc1584fa92880ec72ea6bcee",
+    "url": "/fcc-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "9f90334ca93c0a2f3c55",
+    "url": "/fcc-pomodoro-clock/static/css/main.c732f20c.chunk.css"
+  },
+  {
+    "revision": "9405b272190ea9328c74",
+    "url": "/fcc-pomodoro-clock/static/js/2.c5812aee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-pomodoro-clock/static/js/2.c5812aee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f90334ca93c0a2f3c55",
+    "url": "/fcc-pomodoro-clock/static/js/main.12c49f3a.chunk.js"
+  },
+  {
+    "revision": "f7ea6ec6c4cf3a3fbd54",
+    "url": "/fcc-pomodoro-clock/static/js/runtime-main.0d0832f1.js"
+  },
+  {
+    "revision": "cb5f966d393821565128c99cd98357ba",
+    "url": "/fcc-pomodoro-clock/static/media/Swiss_911_Ultra_Compressed_BT.cb5f966d.ttf"
+  },
+  {
+    "revision": "50f65ebf17383cb19f3b0b1bcf71791d",
+    "url": "/fcc-pomodoro-clock/static/media/alertklaxon_clean.50f65ebf.mp3"
+  }
+]);
