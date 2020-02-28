20b7bd352f2799735b0f9c8e78cde555cfd93b49
Senipah <n.morgan86@googlemail.com>
2020-02-28 17:41:48 +0000

    rename surround to surroundSVG

--- src/Components/LcarsSvg.js
+++ src/Components/LcarsSvg.js
@@ -47,7 +47,7 @@ export const ElbowLeftTop = props => {
   );
 };
 
-const Surround = styled(StyledSvg)`
+const SurroundSVG = styled(StyledSvg)`
   min-height: 80%;
   align-self: center;
   justify-self: center;
@@ -56,13 +56,13 @@ const Surround = styled(StyledSvg)`
 export const SurroundLeft = props => {
   return (
     <SvgContainer>
-      <Surround
+      <SurroundSVG
         viewBox='0 0 3.904897 29.412678'
         xmlns='http://www.w3.org/2000/svg'
         preserveAspectRatio='none'
       >
         <path d='M 0,25.550255 V 3.8624222 H 2.01753 V 25.550255 H 0 m 2.850575,3.862422 H 2.551199 C 1.142257,29.412677 0,27.979719 0,26.211588 v -0.661333 h 2.01753 v 0.981485 c 0.0024,0.884983 0.574154,1.599169 1.2756,1.593463 h 0.611767 v 1.287474 z M 2.850575,0 H 2.5512 C 1.142257,0 0,1.4329586 0,3.2010897 V 3.8624222 H 2.01753 V 2.8809378 C 2.01993,1.9959549 2.591684,1.2817684 3.29313,1.2874741 H 3.904897 V 0 Z' />
-      </Surround>
+      </SurroundSVG>
     </SvgContainer>
   );
 };
@@ -70,7 +70,7 @@ export const SurroundLeft = props => {
 export const SurroundRight = props => {
   return (
     <SvgContainer>
-      <Surround
+      <SurroundSVG
         viewBox='0 0 3.904897 29.412678'
         xmlns='http://www.w3.org/2000/svg'
         preserveAspectRatio='none'
@@ -79,7 +79,7 @@ export const SurroundRight = props => {
           d='M 3.904897,25.550255 V 3.8624222 H 1.887367 V 25.550255 h 2.01753 m -2.850575,3.862422 h 0.299376 c 1.408942,0 2.551199,-1.432958 2.551199,-3.201089 v -0.661333 h -2.01753 v 0.981485 c -0.0024,0.884983 -0.574154,1.599169 -1.2756,1.593463 H 0 v 1.287474 z M 1.054322,0 h 0.299375 c 1.408943,0 2.5512,1.4329586 2.5512,3.2010897 V 3.8624222 H 1.887367 V 2.8809378 C 1.884967,1.9959549 1.313213,1.2817684 0.611767,1.2874741 H 0 V 0 Z'
           // fill={getFill(props.fill)}
         />
-      </Surround>
+      </SurroundSVG>
     </SvgContainer>
   );
 };
